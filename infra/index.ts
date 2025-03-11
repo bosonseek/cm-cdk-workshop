41413c8664108ce08b3e35482034704b3ec9639d
Boson Huang <bhuang@seek.com.au>
2025-03-12 10:45:18 +1100

    renamed

--- infra/index.ts
+++ infra/index.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-new */
 import { App } from 'aws-cdk-lib';
 
-import { AppStack } from './BosonStack';
+import { AppStack } from './AppStack';
 
 const app = new App();
 
-new AppStack(app, 'BosonStack', {
-  stackName: 'BosonStack',
+new AppStack(app, 'AppStack', {
+  stackName: 'AppStack',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
